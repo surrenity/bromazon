bad35c9fdc3e66ea06b34dd3128dae790ed1d877
Surrenity <surren@surrenity.com>
2013-09-28 14:30:21 -0500

    Initialize Respository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bromazon::Application.config.secret_key_base = 'a96c9f87b455a721e82a203f5b0001bdd795e0053b8592855e2b53f64dc2a8b0316305b0541a9a76d85655ba7aa055dfbd33bce2f79dd98b66418dccc4dba094'
